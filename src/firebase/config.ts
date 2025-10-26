380cdd1e85360c346df6dfecd7c47c322a8234a2
Rene Senoner <Senonerrene@gmail.com>
2025-10-26 19:35:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9666948081-8d5a6",
+  "appId": "1:920326861705:web:58b36247cae9ad3724e758",
+  "apiKey": "AIzaSyAJUMpr-9VadO10mHfyTVL-vecypLdjMUc",
+  "authDomain": "studio-9666948081-8d5a6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "920326861705"
+};
